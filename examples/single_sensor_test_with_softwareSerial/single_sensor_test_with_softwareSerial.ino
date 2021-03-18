1563abdac810654abb146e85c351c7391c10fdc0
arzonborz <61315293+arzonborz@users.noreply.github.com>
2021-03-18 03:13:37 +0200

    Update single_sensor_test_with_softwareSerial.ino

--- examples/single_sensor_test_with_softwareSerial/single_sensor_test_with_softwareSerial.ino
+++ examples/single_sensor_test_with_softwareSerial/single_sensor_test_with_softwareSerial.ino
@@ -32,26 +32,26 @@ void setup() {
 
 void loop() {
   // ReadAddress read and retuns the ID address of the SMT100 sensor on the RS485 MODBUS. There should only be one SMT100 sensor connected to the bus at a time because this function uses the broadcast ID address
-  int readaddress = mySensor.ReadAddress();
+  int readaddress = smt100.ReadAddress();
   mySerial.print("readaddress=" + String (readaddress));
   // SetAddress returns a boolean to determine succeful operation. This function Sets the ID address of the SMT100 sensor on the RS485 MODBUS. There should only be one SMT100 sensor connected to the bus at a time because this function uses the broadcast ID address
-  bool setaddress = mySensor.SetAddress(0);
+  bool setaddress = smt100.SetAddress(0);
   mySerial.print("  setaddress=" + String (setaddress));
   // CheckAddress returns a boolean and checks if the Sensor ID is present on RS485 MODBUS
-  bool checkaddress = mySensor.CheckAddress(0);
+  bool checkaddress = smt100.CheckAddress(0);
   mySerial.print("  checkaddress=" + String (checkaddress));
   // reads the data registers from SMT100 sensors with a specified id. returns (-255) when wrong id is passed (-254) when wrong address is passed (-253 & -252) when no correct bytes were recieved
-  int addressID = mySensor.ReadSMT100(0x00,ADDRESS_REG);
+  int addressID = smt100.ReadSMT100(0x00,ADDRESS_REG);
   mySerial.print("  addressID=" + String (addressID));
-  float temp = mySensor.ReadSMT100(0x00,TEMP_REG);
+  float temp = smt100.ReadSMT100(0x00,TEMP_REG);
   mySerial.print("  temp=" + String (temp));
-  float waterContent = mySensor.ReadSMT100(0x00,WATER_CONTENT_REG);
+  float waterContent = smt100.ReadSMT100(0x00,WATER_CONTENT_REG);
   mySerial.print("  waterContent=" + String (waterContent));
-  float permittivity = mySensor.ReadSMT100(0x00,PERMITTIVITY_REG);
+  float permittivity = smt100.ReadSMT100(0x00,PERMITTIVITY_REG);
   mySerial.print("  permittivity=" + String (permittivity));
   // ChangeAddress changes the id address of a given SMT100 id address if the new address is not already taken on the RS485 MODBUS
-  bool changeaddress = mySensor.ChangeAddress(0x00 , 11);
+  bool changeaddress = smt100.ChangeAddress(0x00 , 11);
   mySerial.print("  changeaddress=" + String (changeaddress));
-  int counts = mySensor.ReadSMT100(0x0B,COUNTS_REG);
+  int counts = smt100.ReadSMT100(0x0B,COUNTS_REG);
   mySerial.println("  counts=" + String (counts));
 }
